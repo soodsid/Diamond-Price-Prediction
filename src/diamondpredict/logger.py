17827c26e5cbc00f74c7f61adb5bb2c2d6aa9704
Sidhant <soodsidhant@live.com>
2024-02-24 21:59:42 +0530

    created a logger

--- src/diamondpredict/logger.py
+++ src/diamondpredict/logger.py
@@ -1,4 +1,6 @@
 from loguru import logger
-logger.add('notebooks\logs\log.log', format="{time: YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}.{line} — {message}", level='INFO', colorize=True)
+import os
+os.chdir("../../notebooks")
+logger.add(os.getcwd()+'\logs\log.log', format="{time: YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}.{line} — {message}", level='INFO', colorize=True)
 
 
