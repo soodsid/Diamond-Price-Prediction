609af69dc4284a4f5927b58c4cafabb137aeab00
Sidhant <soodsidhant@live.com>
2024-02-27 21:47:40 +0530

    no longer required

--- src/diamondpredict/logger.py
+++
@@ -1,6 +0,0 @@
-from loguru import logger
-import os
-os.chdir("../../notebooks")
-logger.add(os.getcwd()+'\logs\log.log', format="{time: YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}.{line} — {message}", level='INFO', colorize=True)
-
-
