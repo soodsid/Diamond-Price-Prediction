2a8d8fa180e9cf26756c5d4e4c9ba9c5cf6c286d
Sidhant <soodsidhant@live.com>
2024-02-27 21:47:23 +0530

    added a default class for exceptions in future

---
+++ src/diamondpredict/exception.py
@@ -0,0 +1,16 @@
+"""import sys
+
+class customexp(Exception):
+    def __init__(self, errormsg, errordetail:sys)
+        self.errormsg=errormsg
+#        _,_,self.excta
+
+    def __str__(self) -> str:
+        return super().__str__()
+    """
+
+class exceptt():
+    def __init__(self) -> None:
+        pass
+    def dispay(self):
+        print("dis")
