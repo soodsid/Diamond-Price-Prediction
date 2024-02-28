7dfe4bdb82f844afd9b70d9166cb02580b6fac68
Sidhant <soodsidhant@live.com>
2024-02-28 22:56:55 +0530

    data ingestion classes created

--- src/diamondpredict/components/data_ingestion.py
+++ src/diamondpredict/components/data_ingestion.py
@@ -3,38 +3,43 @@ import numpy as np
 from diamondpredict.loggers import logger
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from pathlib import Path
 import os
+from dotenv import find_dotenv
+
 
 class DataIngestionConfig:
-    rawdata_path = str(os.path('Artifacts/rawdata.csv'))
-    traindata_path = str(os.path('Artifacts/traindata.csv'))
-    testdata_path = str(os.path('Artifacts/testdata.csv'))
+    rawdata_path = str(os.path.join(os.path.dirname(find_dotenv()),'Artifacts','rawdata.csv'))
+    traindata_path = str(os.path.join(os.path.dirname(find_dotenv()), 'Artifacts','traindata.csv'))
+    testdata_path = str(os.path.join(os.path.dirname(find_dotenv()), 'Artifacts','testdata.csv'))
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_paths=DataIngestionConfig()
         
+    @logger.catch()
     def initiate_data_ingestion(self):
         logger.info('data ingestion started')
 
         try:
-            data=pd.read_csv(os.path('notebooks/data/diamonds.csv'))
+            data=pd.read_csv(os.path.join(os.path.dirname(find_dotenv()), 'notebooks', 'data','diamonds.csv'))
             logger.success('data reading succesfull, stored in >> data')
-            os.mkdirs(self.ingestion_paths.rawdata_path, exists=True)
+            os.makedirs(os.path.dirname(self.ingestion_paths.rawdata_path), exist_ok=True)
+
             data.to_csv(self.ingestion_paths.rawdata_path, index=False)
-            logger.success('data saved in artifacts folder')
+            logger.success('rawdata saved in artifacts folder')
 
             logger.info('spliting data in train and test')
             traindata, testdata= train_test_split(data, test_size=0.2)
             logger.success('splitting succesfull')
-            os.mkdirs(self.ingestion_paths.traindata_path, exists=True)
+            os.makedirs(os.path.dirname(self.ingestion_paths.traindata_path), exist_ok =True)
+
             traindata.to_csv(self.ingestion_paths.traindata_path, index=False)
-            os.mkdirs(self.ingestion_paths.testdata_path, exists=True)
+            os.makedirs(os.path.dirname(self.ingestion_paths.testdata_path), exist_ok=True)
+
             testdata.to_csv(self.ingestion_paths.testdata_path, index=False)
             logger.success('train and test data successfuly saved in artifacts folder')
 
             logger.success("successfull data ingestion")
 
         except Exception as e:
-            logger.error(e)
+            logger.exception(e)
