2f7bea18308b78900c9a58cfda258a4b4d996fee
Sidhant <soodsidhant@live.com>
2024-03-03 01:30:57 +0530

    added a utils file

---
+++ src/diamondpredict/utils/utils.py
@@ -0,0 +1,37 @@
+import os
+import pickle
+import numpy as np
+import pandas as pd
+from diamondpredict.loggers import logger
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+
+@logger.catch()
+def saveobject(filepath, obj):
+    try:
+        dirpath=os.path.dirname(filepath)
+        os.makedirs(dirpath, exist_ok=True)
+        with open (filepath,'wb') as fileobj:
+            pickle.dump(obj, fileobj)
+
+    except Exception as e:
+        logger.exception(e)
+
+@logger.catch()
+def evaluate_model(xtrain,ytrain,xtest,ytest, models):       
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(xtrain,ytrain)
+
+            ypred=model.predict(xtest)
+
+            testmodelscore=r2_score(ytest,ypred)
+
+            report[list(models.keys())[i]] = testmodelscore
+
+        return report        
+
+    except Exception as e:
+        logger.exception(e)
