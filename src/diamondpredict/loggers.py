4fbe574884c02166cfe7072e46f58068a35c6a22
Sidhant <soodsidhant@live.com>
2024-03-03 01:29:43 +0530

    stopped info logging

--- src/diamondpredict/loggers.py
+++ src/diamondpredict/loggers.py
@@ -1,8 +1,14 @@
 from loguru import logger
 import os
 from dotenv import find_dotenv
+from datetime import datetime
+
+now = datetime.now()
+filename= now.strftime("%Y-%m-%d_%H.%M.00")+'.log'
+      
 logger.remove()
-logger.add(os.path.join(os.path.dirname(find_dotenv()),'notebooks','log','log.log'), backtrace=True,  diagnose=True, format="-"*200+"\n{time: YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}.{line} — {message}", level='INFO', colorize=False)
+
+logger.add(os.path.join(os.path.dirname(find_dotenv()),'notebooks','log', filename), backtrace=True, diagnose=True, format="-"*200+"\n{time: YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}.{line} — {message}", level='SUCCESS', colorize=False)
 
 #logger.add(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'log','log.log'), backtrace=True,  diagnose=True, format="\n\n\n{time: YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}.{line} — {message}", level='INFO', colorize=False)
 
