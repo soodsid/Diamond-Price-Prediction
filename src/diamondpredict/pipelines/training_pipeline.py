d594bf7d8498e844db61622872a2ba3952f9cc70
Sidhant <soodsidhant@live.com>
2024-03-02 18:58:19 +0530

    debugging in training pipeline

--- src/diamondpredict/pipelines/training_pipeline.py
+++ src/diamondpredict/pipelines/training_pipeline.py
@@ -1,8 +1,14 @@
 from diamondpredict.components import data_ingestion
+from diamondpredict.components import data_transformation
+
 from diamondpredict.loggers import logger
 import os
 import pandas as pd
 
 obj = data_ingestion.DataIngestion()
 
-obj.initiate_data_ingestion()
+trainpath, testpath=obj.initiate_data_ingestion()
+
+newobj = data_transformation.DataTransformation()
+
+newobj.initiate_data_transformation(trainpath, testpath)
