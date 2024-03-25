0d7174881eb290b507721a500273cce3760c4923
sidhant <soodsidhant@live.com>
2024-03-25 12:07:26 +0530

    added model eval in the pipeline

--- src/diamondpredict/pipelines/training_pipeline.py
+++ src/diamondpredict/pipelines/training_pipeline.py
@@ -1,6 +1,7 @@
 from diamondpredict.components import data_ingestion
 from diamondpredict.components import data_transformation
 from diamondpredict.components import model_trainer
+from diamondpredict.components import model_evalutation
 
 
 from diamondpredict.loggers import logger
@@ -8,14 +9,14 @@ import os
 import pandas as pd
 
 ingest = data_ingestion.DataIngestion()
-
 trainpath, testpath=ingest.initiate_data_ingestion()
 
 transform = data_transformation.DataTransformation()
-
 train_arr, test_arr=transform.initiate_data_transformation(trainpath, testpath)
 
 train=model_trainer.ModelTrainer()
-
 train.initiate_model_training(train_arr, test_arr)
+
+eval=model_evalutation.Model_Evaluation()
+eval.initiate_model_eval(train_arr, test_arr)
 
